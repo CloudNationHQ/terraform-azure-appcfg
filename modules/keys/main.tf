8764ee944d7b0db5577d5bb4001751b9d858245c
dkooll <dennis.kool@cloudnation.nl>
2025-03-26 14:35:38 +0100

    fix: allow explicit type specification for app configuration keys

--- modules/keys/main.tf
+++ modules/keys/main.tf
@@ -5,12 +5,13 @@ resource "azurerm_app_configuration_key" "keys" {
       key                 = key.key
       value               = lookup(key, "value", null)
       vault_key_reference = lookup(key, "vault_key_reference", null)
+      type                = lookup(key, "type", key.vault_key_reference != null ? "vault" : "kv")
     }
   }
 
   configuration_store_id = var.configuration_store_id
   key                    = each.value.key
-  type                   = each.value.vault_key_reference != null ? "vault" : "kv"
+  type                   = each.value.type
   value                  = each.value.vault_key_reference == null ? each.value.value : null
   vault_key_reference    = each.value.vault_key_reference
 }
